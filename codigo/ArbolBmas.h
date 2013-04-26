44a813cf06c21abd63a74f465f29ac930398e011
Federico Rossi <federicomrossi@gmail.com>
2013-04-26 01:43:27 -0300

    Hechas las declaraciones de Arbol B+ e Indice. Falta terminar de documentar.

--- codigo/ArbolBmas.h
+++ codigo/ArbolBmas.h
@@ -1,23 +1,90 @@
+//
+//  ArbolBmas.h
+//	[ Insertar documentacion ]
+//
+
+#ifndef ARBOLBMAS_H
+#define ARBOLBMAS_H
+
+
+#include <iostream>
+#include "VarBuffer.h"
+
+
+
+
 /* ****************************************************************************
- * ****************************************************************************
- * Clase ArbolBMas
- * ............................................................................
- * Implementación de la clase que modela un arbol B+.
- *
- * ****************************************************************************
+ * DECLARACIÓN DE LA CLASE
  * ***************************************************************************/
 
 
+template < typename TipoClave >
+class ArbolBmas
+{
 
-#ifndef ARBOL_BMAS_H
-#define ARBOL_BMAS_H
+private:
 
+	struct Nodo 
+	{
+		int maxClaves;		// Numero maximo de claves en el nodo
 
+		// Constructor
+		Nodo(int maxClaves, int unico = 1);
 
-/* ****************************************************************************
-* DECLARACIÓN DE LA CLASE
-* ***************************************************************************/
+		//
+		int insertar(const TipoClave clave, int direccionRegistro);
 
+		//
+		int eliminar(const TipoClave clave, int direccionRegistro = -1);
+
+		// Devuelve la mayor clave del nodo
+		int claveMayor();
+
+		//
+		int particionar(Nodo *nodoNuevo);
+	};	
+
+	Nodo raiz;				// Nodo de la raiz
+	int altura;				// Altura del arbol
+	int orden;				// Orden del arbol
+	Nodo *Nodos[];			// Almacenador para rama
+	VarBuffer archivo;		// Archivo de almacenamiento del arbol
+
+	//
+	Nodo* buscarHoja(const TipoClave clave);
+
+	// Carga un nodo en memoria
+	Nodo* cargarNodo(const int direccionRegistro);
+
+	// Almacena un nodo en archivo
+	int almacenarNodo(Nodo *nodo);
+
+public:
+
+	// Constructor
+	ArbolBmas(int orden, int tamanioClave = sizeof(TipoClave), int unico = 1);
+
+	// Destructor
+	~ArbolBmas();
+
+	// CONSIDERAR BORRAR ESTE METODO
+	int iniciar(string nombre, int modo);
+
+	// CONSIDERAR BORRAR ESTE METODO
+	int crear(string nombre, int modo);
+
+	// CONSIDERAR BORRAR ESTE METODO
+	int cerrar();
+
+	//
+	int insertar(const TipoClave clave, const int direccionRegistro);
+
+	//
+	int eliminar(const TipoClave clave, const int direccionRegistro = -1);
+
+	//
+	int buscar(const TipoClave clave, const int direccionRegistro = -1);
+};
 
 
 
