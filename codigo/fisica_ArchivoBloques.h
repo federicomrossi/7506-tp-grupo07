db47e6610ff80d69e1b1a89130783c41133055ca
Federico Colangelo <federico.colangelo@semperti.com>
2013-05-12 13:39:34 -0300

    Optimizacion de diseño de ArchivoBloques
    Se utiliza *void en lugar de VarBuffer como parametro de escribir y
    leer.

--- codigo/fisica_ArchivoBloques.h
+++ codigo/fisica_ArchivoBloques.h
@@ -3,8 +3,8 @@
 //  EL PRIMER BLOQUE ES EL BLOQUE 1
 //  Clase encargada de manejar la estructura de archivos en bloques de registros.
 //  Su función sera llevar registro de la ocupación de los bloques y del posicionamiento de los mismos.
-//  Para escribir en el archivo debera pasarse como argunmento un objeto VarBuffer y el numero de bloque en el cual escribirlo.
-//  Para leer deberá pasarse el numero de bloque a leer y un VarBuffer en donde poner los datos leidos.
+//  Para escribir en el archivo debera pasarse como argunmento un puntero a un objeto cualquiera y el numero de bloque en el cual escribirlo.
+//  Para leer deberá pasarse el numero de bloque a leer y un puntero a un objeto cualquier en donde poner los datos leidos.
 //  Created by Federico Colangelo on 29/04/13.
 //  Copyright (c) 2013 Federico Colangelo. All rights reserved.
 //
@@ -25,27 +25,26 @@ public:
     void cerrar();
     
     /*
-    // PRE: El tamano del VarBuffer debera ser menor o igual al tamaño del bloque.
-    // USO: El metodo escribir bloque utiliza el VarBuffer que es le pasa por referencia para escribirlo en el bloque correspondiente.
-    // Para ello previamente debe empaquetarse el contenido a guardar en el bloque dentro de un VarBuffer mediante metodos de dicha clase.
-    // POST: Se escribe el contenido de VarBuffer en el bloque elegido.
+    // PRE: El tamano del VarBuffer debera ser menor o igual al tamano del bloque.
+    // USO: El metodo escribir bloque utiliza la direccion de objeto que se le pasa para escribirlo en el bloque correspondiente.
+    // POST: Se escribe el contenido del objeto en el bloque elegido.
     // Este metodo no cierra el arhcivo.
     */
-    int escribirBloque(const VarBuffer &bloque, unsigned int numeroBloque);
+    int escribirBloque(const void* registro, unsigned int numeroBloque);
     
     /*
-    // PRE: El tamano de VarBuffer debera ser mayor o igual que el del bloque
-    // USO: La clase que desee utilizar el archivo por bloques deberá pasar el VarBuffer donde quiere que se lea la informacion.
-    // POST: El metodo escribe el contenido del bloque en VarBuffer
+    // USO: La clase que desee utilizar el archivo por bloques debera pasar la direccion de un objeto en donde quiere que se lea la informacion.
+    // POST: El metodo escribe el contenido del bloque en el objeto
     // Este metodo no cierra el archivo.
     */
-    int leerBloque(VarBuffer &bloque, unsigned int numeroBloque);
+    int leerBloque(void* registro, unsigned int numeroBloque);
     
     ~ArchivoBloques();
 
 private:
     unsigned int blockSize;
     unsigned short int primerBloque;
+    VarBuffer IObuffer;
     char* filename;
 
     //int extenderMapaOcupacion();
