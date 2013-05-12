687da2012424943afcf2d51cc709462dac2d95fa
Federico Colangelo <federico.colangelo@semperti.com>
2013-05-12 12:55:10 -0300

    Cambio logico en ArchivoBloques.
    En función de lo discutido el 11/05/13 se cambia la estructura de esta
    clase para que lo único que haga se escribir o leer un bloque de
    información de un archivo binario.

--- codigo/fisica_ArchivoBloques.h
+++ codigo/fisica_ArchivoBloques.h
@@ -1,51 +1,54 @@
 //
 //  BlockFile.h
-//  FileHandler
-//  Clase encargada de manejar la estructura de archivos en bloques de registros. Esta clase definira un template para que cualquier clase pueda grabar sus registros.
+//  EL PRIMER BLOQUE ES EL BLOQUE 1
+//  Clase encargada de manejar la estructura de archivos en bloques de registros.
 //  Su función sera llevar registro de la ocupación de los bloques y del posicionamiento de los mismos.
-//  La clase "T" que define el tipo de registros que guardara el archivo debera implementar los metodos pack y unpack con la siguiente firma
-//  int pack(VarBuffer &unBuffer);
-//  int unpack(VarBuffer &unBuffer);
-//  que recibiendo por referencia un objeto de tipo VarBuffer sepan empaquetarse dentro del buffer para ser persistidos y luego desempaquetarse
-//  al momento de leer el bloque
-//
+//  Para escribir en el archivo debera pasarse como argunmento un objeto VarBuffer y el numero de bloque en el cual escribirlo.
+//  Para leer deberá pasarse el numero de bloque a leer y un VarBuffer en donde poner los datos leidos.
 //  Created by Federico Colangelo on 29/04/13.
 //  Copyright (c) 2013 Federico Colangelo. All rights reserved.
 //
 
-#ifndef ArchivoBloques_h
-#define ArchivoBloques_h
+#ifndef fisica_ArchivoBloques_h
+#define fisica_ArchivoBloques_h
 
 #include <iostream>
 #include <fstream>
 #include "fisica_VarBuffer.h"
 
-template < class TipoRegistro >
 class ArchivoBloques
 {
 public:
 
-    ArchivoBloques(unsigned int blockSize, char* filename, 
-        float cargaMaxima = 0.7);
-
-    void abrir(char* filename);
-
+    ArchivoBloques(unsigned int blockSize, char* filename);
+    
     void cerrar();
     
-    // [DOCUMENTAR!]
-    int escribirBloque(const TipoRegistro *registro,unsigned int numeroBloque);
+    /*
+    // PRE: El tamano del VarBuffer debera ser menor o igual al tamaño del bloque.
+    // USO: El metodo escribir bloque utiliza el VarBuffer que es le pasa por referencia para escribirlo en el bloque correspondiente.
+    // Para ello previamente debe empaquetarse el contenido a guardar en el bloque dentro de un VarBuffer mediante metodos de dicha clase.
+    // POST: Se escribe el contenido de VarBuffer en el bloque elegido.
+    // Este metodo no cierra el arhcivo.
+    */
+    int escribirBloque(const VarBuffer &bloque, unsigned int numeroBloque);
     
-    TipoRegistro* leerBloque(unsigned int numeroBloque);
+    /*
+    // PRE: El tamano de VarBuffer debera ser mayor o igual que el del bloque
+    // USO: La clase que desee utilizar el archivo por bloques deberá pasar el VarBuffer donde quiere que se lea la informacion.
+    // POST: El metodo escribe el contenido del bloque en VarBuffer
+    // Este metodo no cierra el archivo.
+    */
+    int leerBloque(VarBuffer &bloque, unsigned int numeroBloque);
     
     ~ArchivoBloques();
 
 private:
-    unsigned int primerBloque;
-    unsigned int* mapaOcupacion;
-    float cargaMaxima;
-    VarBuffer buffer;
+    unsigned int blockSize;
+    unsigned short int primerBloque;
+    char* filename;
 
-    int extenderMapaOcupacion();
+    //int extenderMapaOcupacion();
 };
 
 #endif
