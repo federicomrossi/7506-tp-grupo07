191f66ad1776718c3e1ec358afdec0135f4f2b32
pablisho <rodriguez.pabloariel@gmail.com>
2013-05-14 01:53:31 -0300

    Agregadas clase de dominio de par string, int.

--- codigo/logica_IndiceTitulo.h
+++ codigo/logica_IndiceTitulo.h
@@ -7,8 +7,8 @@ class IndiceTitulo{
     public:
         IndiceTitulo();
         ~IndiceTitulo();
-        int agregar(int posArch, std::string titulo);
-        int recuperar(std::string titulo, int **posArch);
+        int agregar(unsigned int posArch, std::string titulo);
+        int recuperar(std::string titulo, unsigned int* posArch);
         int eliminarTodo();
     private:
         //UN ARBOL
