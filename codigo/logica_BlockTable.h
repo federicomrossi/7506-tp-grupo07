acfafbad5edc1de8ca32da0f6506e5b72ead0a71
rburdet <rburdet@rburdet-K53SC.(none)>
2013-06-08 20:14:05 -0300

    Bug 8 Fixed

--- codigo/logica_BlockTable.h
+++ codigo/logica_BlockTable.h
@@ -47,7 +47,7 @@ class BlockTable{
 		bool canAddBlock(Block<T>* aBlock); //Tengo referencias libres? true significa que no hace falta duplicar
 		void redisperse(Block<T>* anOldBlock,Block<T>* aNewBlock); //redisperso los registros del bloque viejo al bloque nuevo
 
-		void actualizar(T* & newReg);
+		void actualizar(T & newReg);
 
 		void prueba(Block<T>*);
 	protected:
@@ -57,6 +57,7 @@ class BlockTable{
 		char *blockPath;
 		int blockSize;
 		bool firstTime;
+		unsigned int numberOfDuplications;
 };
 
 template <class T>
@@ -69,6 +70,7 @@ BlockTable<T>::BlockTable(string path, string blockPath, int blockSize) {
 	this->blockSize = blockSize;
 	this->firstTime = true;
 	this->read();
+	this->numberOfDuplications= 0;
 }
 
 template <class T>
@@ -150,7 +152,13 @@ int BlockTable<T>::insert(T *aReg){
 				return -1;
 			}
 			if (! this->canAddBlock(&tmpBlock)){
-				this->duplicateTable();
+				if (this->numberOfDuplications<MAX_DUPLICATION_TIMES){
+					this->duplicateTable();	
+					this->numberOfDuplications++;
+				}else{
+					cerr<<"ERROR AGREGANDO REGISTRO, CAMBIE EL TAMANIO DEL BLOQUE"<<endl;
+					exit(EXIT_FAILURE);
+				}
 			}
 			int lastBlockNum = tmpBlock.newBlockNum();
 			Block<T> anotherBlock(tmpBlock.duplicateDispersionSize(), lastBlockNum, this->blockPath, this->blockSize);
@@ -206,12 +214,12 @@ void BlockTable<T>::redisperse(Block<T>* anOldBlock, Block<T>* aNewBlock){
 
 
 template <class T>
-void BlockTable<T>::actualizar(T* & newReg){
+void BlockTable<T>::actualizar(T & newReg){
 	this->read();
-	int pos = HashExtensible::doHash(newReg->getClave(),this->getSize());
+	int pos = HashExtensible::doHash(newReg.getClave(),this->getSize());
 	int tmpBlockNumber = this->blockReferences[pos];
 	// No me importa el tamanio de dispersion
-	Block<T> tmpBlock(0,tmpBlockNumber,this->blockSize);
+	Block<T> tmpBlock(0,tmpBlockNumber,this->blockPath,this->blockSize);
 	tmpBlock.read();
 	tmpBlock.actualizar(newReg);
 }
@@ -219,7 +227,7 @@ void BlockTable<T>::actualizar(T* & newReg){
 template <class T>
 void BlockTable<T>::duplicateTable(){
 	int newBlockTableSize = this->size *2;
-	int *tmpBlockReference = new int[newBlockTableSize];//(int*) calloc(newBlockTableSize, sizeof(int));
+	int *tmpBlockReference = new int[newBlockTableSize];
 	for (int i = 0; i < this->size ; i++) {
 		tmpBlockReference[i] = this->blockReferences[i];
 		tmpBlockReference[this->size+i] = this->blockReferences[i];
